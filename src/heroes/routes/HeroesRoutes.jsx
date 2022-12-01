a982ccfc2c0503e010b391c4126c30fef49813d1
giselars <gisela-rs@outlook.com>
2022-12-01 16:52:49 +0100

    :art: add second route and pages

---
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -0,0 +1,24 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import { Navbar } from "../../ui";
+import { MarvelPage, DcPage, SearchPage, HeroPage} from "../../heroes";
+
+
+export const HeroesRoutes = () => {
+  return (
+    <>
+        <Navbar />
+        <div className="container">
+            <Routes>
+                <Route path="marvel" element={<MarvelPage/>}/>
+                <Route path="dc" element={<DcPage/>}/>
+                
+                <Route path="search" element={<SearchPage/>}/>
+                <Route path="hero" element={<HeroPage/>}/>
+
+                <Route path="/" element={<Navigate to='/marvel'/>}/>
+            </Routes>
+        </div>
+    </>
+  )
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,15 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
-import { DcPage, MarvelPage } from "../heroes";
+import { HeroesRoutes } from "../heroes";
 import { LoginPage } from "../auth";
-import { Navbar } from "../ui";
-
-
 
 
 export const AppRouter = () => {
   return (
     <>
-        <Navbar/>
         <Routes>
-            <Route path="marvel" element={<MarvelPage/>}/>
-            <Route path="dc" element={<DcPage/>}/>
             <Route path="login" element={<LoginPage/>}/>
-
-            <Route path="/" element={<Navigate to='/marvel'/>}/>
+            <Route path="/*" element={<HeroesRoutes/>}/>
         </Routes>
     </>
   )
