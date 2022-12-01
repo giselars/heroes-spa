c0821bbff804a4aceadfed1e5ff6b18a8e51944c
giselars <gisela-rs@outlook.com>
2022-12-01 17:11:12 +0100

    :zap: hook useNavigate second router

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,16 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+    
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    }
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
             
@@ -37,6 +46,7 @@ export const Navbar = () => {
                         Gisela
                     </span>
                     <button
+                    onClick={ onLogout }
                     className='nav-item nav-link btn'
                     >
                         Logout
