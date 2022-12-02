72596ad7a2c60ff0bba8913704b6de874ebac633
giselars <gisela-rs@outlook.com>
2022-12-02 11:22:21 +0100

    :lipstick: add styles on HeroScreen component

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,55 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+
+// const {id,...rest} = useParams();
+const {id} = useParams();
+const hero = getHeroById(id);
+const navigate = useNavigate();
+
+const onNavigateBack = () => {
+    navigate(-1);
+}
+
+if (!hero){
+  return <Navigate to={"/marvel"}/>
+}
+
   return (
-    <h1>HeroPage</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img 
+            src={`/assets/heroes/${id}.jpg`} 
+            alt={ hero.superhero } 
+            className="img-thumbnail" />
+      </div>
+      
+      <div className="col-8">
+        <h3>{ hero.superhero }</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b>{ hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b>{ hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b>{ hero.first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-5">
+          Characters
+        </h5>
+        <p>{ hero.characters }</p>
+
+        <button 
+          className="btn btn-outline-primary"
+          onClick={ onNavigateBack }
+          >
+          Regresar
+        </button>
+      </div>
+    </div>
   )
 }
