55fb614f33edd933fc11c7be6e06cfa53736b260
giselars <gisela-rs@outlook.com>
2022-12-02 12:01:00 +0100

    :zap: add hook useMemo to improve performance

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,10 +1,11 @@
+import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers"
 import { HeroCard } from "./HeroCard";
 
 
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublisher(publisher);
+    const heroes = useMemo(() => getHeroesByPublisher(publisher),[publisher]);
     
     return (
         <div className="row rows-cols-1 row-cols-md-3 g-3">
